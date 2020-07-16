8e49af804f9e9ebd679a9af8b1c2c2c6f7016d1d
SYW23 <775619610@qq.com>
2020-07-16 22:42:10 +0800

    9.3结束

--- hello_Cpp/chapter9.cpp
+++ hello_Cpp/chapter9.cpp
@@ -1,6 +1,7 @@
 # include <iostream>
 # include "hello_Cpp.h"
 # include <vector>
+# include <list>
 # include <array>
 using namespace std;
 
@@ -8,7 +9,7 @@ int chapter9()
 {
 	vector<int> v1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-	vector<int> v2(v1.begin(), v1.begin()+4);    // 使用已定义的容器的子序列初始化新定义的容器（区间左闭右开）
+	vector<int> v2(v1.begin(), v1.begin() + 4);    // 使用已定义的容器的子序列初始化新定义的容器（区间左闭右开）
 	for (auto& i : v1)
 	{
 		cout << i << " ";
@@ -20,6 +21,20 @@ int chapter9()
 		cout << i << " ";
 	}
 	cout << endl;
+
+	// vector和string不支持push_front和pop_front
+	// 利用insert的返回值在指定位置反复插入元素，并保证永远在首元素前面插入新元素
+	list<string> lst{ "default" };
+	auto beg = lst.begin();
+	string word;
+	while (cin >> word)
+	{
+		beg = lst.insert(beg, word);
+	}
+	for (auto& i : lst)
+	{
+		cout << i << endl;
+	}
 
 	// array初始化：必须指定元素类型和容器大小
 	array<int, 30> arr;
